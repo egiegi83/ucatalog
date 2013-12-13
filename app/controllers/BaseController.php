c4c14916838f5bfe28c9a85bc5bde3867660924d
Francesco Esposito <fraesps@gmail.com>
2013-12-13 15:56:49 +0100

    Modifica BaseController
    aggiunti due metodi:
    getDipartimenti()
    getAreaScientifica()
    
    sono visibili tutti i contoller con  es. “$this->getDipartimenti()”
    
    restituiscono un array già formattato per le le select dinamiche.
    Se passiamo ad una view il valore di ritorno di una di queste funzioni,
    per esempio nella variabile $dipartimenti, possiamo fare:
    
    {{ Form::select('dipartimento', $dipartimenti) }}
    
    e abbiamo la select con i valori del DB

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -15,4 +15,36 @@ class BaseController extends Controller {
 		}
 	}
 
+	/**
+	*	Restituisce un array di tutti i dipartimenti
+	*	presenti nel database (mappati per ID). 
+	*	utilizzato per la select nelle view di modifica e inserimento utente
+	*	@return array
+	*/
+	public static function getDipartimenti(){
+		$dipArray = array();
+		$dipArray[''] = 'Seleziona dipartimento';
+		foreach(Dipartimento::all() as $dipartimento){
+			$dipArray[$dipartimento->getId()] = $dipartimento->getSigla() . " - " . $dipartimento->getNome(); 
+		}
+		return $dipArray;
+	}
+
+	/**
+	*	Restituisce un array di tutte le aree di ricerca
+	*	presenti nel database (mappati per ID). 
+	*	utilizzato per la select nelle view di modifica e inserimento utente
+	*	@return array
+	*/
+	public static function getAreaScientifica(){
+		$areaArray = array();
+		$areaArray[''] = 'Seleziona area di ricerca';
+		foreach(AreaScientifica::all() as $area_scientifica){
+			$areaArray[$area_scientifica->getId()] = $area_scientifica->getNome();
+		}
+		return $areaArray;
+	}
+
 }
+
+?>
